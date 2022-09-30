f20f11ab85e95d379908d05e14c570528e1b4b56
DiscordMp3 <113410288+DiscordMp3@users.noreply.github.com>
2022-09-30 09:53:41 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+	AOS.init();	
+});
+wow = new WOW(
+	{
+	boxClass:     'wow',      // default
+	animateClass: 'animated', // default
+	offset:       0,          // default
+	mobile:       false,       // default
+	live:         true        // default
+  }
+  )
+  wow.init();
